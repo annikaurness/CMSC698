f5dbdfe0d3a72fad066f760a3c432187e534d775
Annika Urness <annikaurness@Annikas-MacBook-Air-3.local>
2025-11-10 15:47:50 -0600

    switch to google spreadsheets api

--- search.js
+++ search.js
@@ -1,1 +1,13 @@
-//something something if location= off campus, show walking/driving distance options
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const creds = require('.study-buddy-477821-5207d4afcac2.json'); // Your service account key
+
+async function accessSpreadsheet() {
+const doc = new GoogleSpreadsheet('Study Locations Data');
+    await doc.useServiceAccountAuth(creds);
+  await doc.loadInfo(); // loads document properties and worksheets
+  const sheet = doc.sheetsByIndex[0]; // or by name: doc.sheetsByTitle['Sheet1']
+    const rows = await sheet.getRows();
+    console.log(rows);
+}
+accessSpreadsheet();
